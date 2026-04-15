38f453623282ea028850463553f0fc7e49a3a0a6
Dinesh K <dinesh.reachme@gmail.com>
2026-04-15 20:14:47 +0530

    android app - correction

--- android-twa/app/src/main/java/com/eventise/twa/LauncherActivity.java
+++ android-twa/app/src/main/java/com/eventise/twa/LauncherActivity.java
@@ -1,4 +1,5 @@
 package com.eventise.twa;
 
 public class LauncherActivity extends com.google.androidbrowserhelper.trusted.LauncherActivity {
+    // The library will automatically use the URL defined in the Manifest/strings.xml
 }
